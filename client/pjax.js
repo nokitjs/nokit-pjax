595897ed3ac74f7793340f887c4fea0495429a31
Houfeng <admin@xhou.net>
2016-01-08 14:54:34 +0800

    dev

--- client/pjax.js
+++ client/pjax.js
@@ -28,19 +28,28 @@
     //呈现
     owner.render = function (result) {
         owner.elements = owner.elements || {};
+        //更新容器内容
         for (var key in result) {
-            owner.elements[key] = owner.elements[key] || $(key);
-            owner.elements[key].html(result[key]);
+            if (key != 'title' && result[key] !== null && result[key] !== undefined) {
+                owner.elements[key] = owner.elements[key] || $(key);
+                owner.elements[key].html(result[key]);
+            }
+        }
+        //更新标题
+        if (result.title !== null && result.title !== undefined) {
+            document.title = result.title;
         }
     };
 
     //更新地址和标题
-    owner.pushState = function (url) {
-        window.history.pushState({}, "", url);
+    owner.pushState = function (url, result) {
+        result = result || {};
+        window.history.pushState(result, result.title, url);
     };
 
     //绑定事件
     $(function () {
+        //链接事件
         $(document).on(owner.EVENT_NAME, '[' + owner.CONTAINER_ATTR_NAME + ']', function (event) {
             var link = $(this);
             var url = link.attr(owner.URL_ATTR_NAME) || link.attr('href');
@@ -50,9 +59,13 @@
             }
             owner.request(url, containers, function (result) {
                 owner.render(result);
-                owner.pushState(url);
+                owner.pushState(url, result);
             });
             return false;
+        });
+        //state 改变事件
+        $(window).on('popstate', function (event) {
+            owner.render(window.history.state);
         });
     });
 
